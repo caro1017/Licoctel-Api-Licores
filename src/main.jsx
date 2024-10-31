6844ca2591b2b4139c49158fc044c1e8f3177bd7
Carolina Uribe Botero <uribebotero.carolin@gmail.com>
2024-10-30 20:50:54 -0500

    Ajustes iniciales e instalacion de Material UI, Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+/* Estilos Material UI en Segundo plano */
+import { StyledEngineProvider } from "@mui/material";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
+  </StrictMode>
+);
